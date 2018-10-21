ba136df82d8f4d1678d59dd2ef7726b6874fde8f
Renan Seni <renanseni@gmail.com>
2018-10-21 17:36:57 -0300

    Criado calculadora

---
+++ restful-api/restful-api/Controllers/CalculatorController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace restful_api.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Calculator")]
+    public class CalculatorController : Controller
+    {
+        [HttpGet("{firstNumber}/{secondNumber}")]
+        public IActionResult Sum(string firstNumber, string secondNumber)
+        {
+
+            if (isNumeric(firstNumber) && isNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+
+            return BadRequest("");
+        }
+
+        private decimal ConvertToDecimal(string number)
+        {
+            decimal decimalValue;
+            if (decimal.TryParse(number, out decimalValue))
+            {
+                return decimalValue;
+            }
+
+            return 0;
+        }
+
+        private bool isNumeric(string strNumber)
+        {
+            double number;
+            return double.TryParse(strNumber, System.Globalization.NumberStyles.Any, System.Globalization.NumberFormatInfo.InvariantInfo, out number);
+        }
+    }
+}
